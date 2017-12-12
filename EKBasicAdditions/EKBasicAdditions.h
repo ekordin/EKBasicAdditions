8efd9f61107bfc649c6afa0177100e4540e53723
Eugene Kordin <kordin@MacBook-Pro-0xEE.local>
2017-12-12 21:23:18 +0200

    New project skeleton.

---
+++ EKBasicAdditions/EKBasicAdditions.h
@@ -0,0 +1,19 @@
+//
+//  EKBasicAdditions.h
+//  EKBasicAdditions
+//
+//  Created by Eugene Kordin on 11/12/16.
+//  Copyright © 2016 Eugene Kordin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EKBasicAdditions.
+FOUNDATION_EXPORT double EKBasicAdditionsVersionNumber;
+
+//! Project version string for EKBasicAdditions.
+FOUNDATION_EXPORT const unsigned char EKBasicAdditionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EKBasicAdditions/PublicHeader.h>
+
+
