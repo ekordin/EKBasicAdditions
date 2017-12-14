6ba0621a4cce53982e5ed6a7b6e98dff11bd8f89
Eugene Kordin <kordin@MacBook-Pro-0xEE.local>
2017-12-14 22:29:12 +0200

    Added new functions.

--- EKBasicAdditions/NSApplication+Additions.h
+++ EKBasicAdditions/NSApplication+Additions.h
@@ -17,4 +17,17 @@
 + (BOOL)systemVersionIsLessThan:(NSUInteger)version;
 - (BOOL)systemVersionIsLessThan:(NSUInteger)version;
 
++ (NSString*)applicationVersion;
+- (NSString*)applicationVersion;
+
++ (NSString*)applicationIdentifier;
+- (NSString*)applicationIdentifier;
+
++ (NSString*)applicationName;
+- (NSString*)applicationName;
+
++ (NSObject<NSApplicationDelegate>*)applicationDelegate;
+
+- (void)beginSheet:(NSWindow*)sheet modalForWindow:(NSWindow*)modalWindow didEndBlock:(void (^)(NSInteger returnCode))block;
+
 @end
--- EKBasicAdditions/NSApplication+Additions.m
+++ EKBasicAdditions/NSApplication+Additions.m
@@ -30,4 +30,55 @@
     return [[self class] systemVersionIsLessThan:version];
 }
 
++ (NSString *)applicationVersion
+{
+    return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+}
+
+- (NSString *)applicationVersion
+{
+    return [[self class] applicationVersion];
+}
+
++ (NSString *)applicationIdentifier
+{
+    return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
+}
+
+- (NSString *)applicationIdentifier
+{
+    return [[self class] applicationIdentifier];
+}
+
++ (NSString *)applicationName
+{
+    return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleExecutable"];
+}
+
+- (NSString *)applicationName
+{
+    return [[self class] applicationName];
+}
+
++ (NSObject<NSApplicationDelegate>*)applicationDelegate
+{
+    return [NSApplication sharedApplication].delegate;
+}
+
+- (void)beginSheet:(NSWindow*)sheet modalForWindow:(NSWindow*)modalWindow didEndBlock:(void (^)(NSInteger returnCode))block
+{
+    [self beginSheet:sheet
+      modalForWindow:modalWindow
+       modalDelegate:self
+      didEndSelector:@selector(sheet_blockSheetDidEnd:returnCode:contextInfo:)
+         contextInfo:(__bridge void * _Null_unspecified)(block)];
+}
+
+- (void)sheet_blockSheetDidEnd:(NSWindow*)__unused sheet returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo
+{
+    void (^block)(NSInteger returnCode) = (__bridge void (^)(NSInteger))(contextInfo);
+    block(returnCode);
+    block = nil;
+}
+
 @end
